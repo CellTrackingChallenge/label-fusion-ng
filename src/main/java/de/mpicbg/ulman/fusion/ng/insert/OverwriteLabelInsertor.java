288af0844e8cf44d7c5742e486018d3d7883cc9a
Vladimír Ulman <ulman@mpi-cbg.de>
2020-04-21 23:27:32 +0200

    ADD: fusion.OverwriteLabelInsertor() - just plain label insertor

---
+++ src/main/java/de/mpicbg/ulman/fusion/ng/insert/OverwriteLabelInsertor.java
@@ -0,0 +1,48 @@
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2020, Vladimír Ulman
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package de.mpicbg.ulman.fusion.ng.insert;
+
+import net.imglib2.img.Img;
+import net.imglib2.loops.LoopBuilder;
+import net.imglib2.type.numeric.IntegerType;
+import net.imglib2.type.numeric.RealType;
+
+public class OverwriteLabelInsertor<LT extends IntegerType<LT>, ET extends RealType<ET>>
+implements LabelInsertor<LT,ET>
+{
+	@Override
+	public
+	void insertLabel(final Img<ET> inSingleLabelImg,
+	                 final Img<LT> outResultImg,
+	                 final int outMarker,
+	                 final InsertionStatus status)
+	{
+		LoopBuilder.setImages(outResultImg,inSingleLabelImg).forEachPixel(
+			(io,lab) -> { if (lab.getRealFloat() > 0) io.setInteger(outMarker); } );
+	}
+}
