c3deb4ef530e08ebf298892d51d7c5d3777584f2
Vladimír Ulman <ulman@fi.muni.cz>
2022-01-04 01:15:35 +0100

    FIX: CollisionsManagingLabelInsertor.registerPxInCollision() warns when      underlying image map cannot hold more PxCoords (pixels at some intersection)

--- src/main/java/de/mpicbg/ulman/fusion/ng/insert/CollisionsManagingLabelInsertor.java
+++ src/main/java/de/mpicbg/ulman/fusion/ng/insert/CollisionsManagingLabelInsertor.java
@@ -108,6 +108,9 @@ implements LabelInsertor<LT,ET>
 		p.claimingLabels.add( claimer );
 		pxInINTERSECTION.add( p );
 		pxInINTERSECTION_map_RA.get().setInt( pxInINTERSECTION.size() );
+		//
+		if (pxInINTERSECTION.size() == pxInINTERSECTION_map.firstElement().getMaxValue())
+			System.out.println("WARNING: pxInINTERSECTION map cannot hold more PxCoords!");
 	}
 
 	List<PxCoord> pxInINTERSECTION;
