3228ec608db0fe785e5f0dff8cfa48a2b5b20cad
Vladimír Ulman <ulman@fi.muni.cz>
2022-01-06 22:02:17 +0100

    COS: license headers in .java files updated copyright year

--- src/main/java/de/mpicbg/ulman/fusion/ng/postprocess/KeepLargestCCALabelPostprocessor.java
+++ src/main/java/de/mpicbg/ulman/fusion/ng/postprocess/KeepLargestCCALabelPostprocessor.java
@@ -1,7 +1,7 @@
 /*
  * BSD 2-Clause License
  *
- * Copyright (c) 2020, Vladimír Ulman
+ * Copyright (c) 2020,2022, Vladimír Ulman
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
