ec9a2f8c894afd8a29a89536bd96fb1220eaacec
Vladimír Ulman <ulman@fi.muni.cz>
2022-02-09 18:12:13 +0100

    FIX: top-level classes ain't using souts (but are logging instead)

--- src/main/java/de/mpicbg/ulman/fusion/ng/AbstractWeightedVotingFusionAlgorithm.java
+++ src/main/java/de/mpicbg/ulman/fusion/ng/AbstractWeightedVotingFusionAlgorithm.java
@@ -249,17 +249,6 @@ implements WeightedVotingFusionAlgorithm<IT,LT>
 				//found a new marker, determine its size and the AABB it spans
 				MajorityOverlapBasedLabelExtractor.findAABB(mCursor, minBound,maxBound);
 				log.warn("found its AABB");
-/*
-				//report detected markers just for debug
-				System.out.print("marker "+mCursor.get().getInteger()+": lower corner: (");
-				for (int d=0; d < minBound.length-1; ++d)
-					System.out.print(minBound[d]+",");
-				System.out.println(minBound[minBound.length-1]+")");
-				System.out.print("marker "+mCursor.get().getInteger()+": upper corner: (");
-				for (int d=0; d < maxBound.length-1; ++d)
-					System.out.print(maxBound[d]+",");
-				System.out.println(maxBound[maxBound.length-1]+")");
-*/
 
 				//sweep over all input images
 				selectedInImgs.clear();
@@ -273,7 +262,6 @@ implements WeightedVotingFusionAlgorithm<IT,LT>
 							Views.interval(inImgs.get(i), minBound,maxBound),
 							Views.interval(markerImg,     minBound,maxBound),
 							curMarker);
-					//System.out.println(i+". image: found label "+matchingLabel);
 					log.warn("finished the searching, found "+matchingLabel);
 
 					if (matchingLabel > 0)
