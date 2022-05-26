7da7e19e4cbda73e4794f042a0bf7e2cac5dd868
Vladimír Ulman <ulman@fi.muni.cz>
2022-05-26 13:33:21 +0200

    ADD1/3: DetSegCumulativeScores DET related stuff - definition

--- src/main/java/de/mpicbg/ulman/fusion/util/DetSegCumulativeScores.java
+++ src/main/java/de/mpicbg/ulman/fusion/util/DetSegCumulativeScores.java
@@ -14,9 +14,27 @@ public class DetSegCumulativeScores
 		addSegMatch(0.0);
 	}
 
+	public void addDetFalseNegative() {
+		++detFnCnt;
+		++sectionDetFnCnt;
+	}
+
+	public void addDetFalsePositive() {
+		++detFpCnt;
+		++sectionDetFpCnt;
+	}
+
+	public void addDetTruePositive() {
+		++detTpCnt;
+		++sectionDetTpCnt;
+	}
+
 	// --------------- global score ---------------
 	private double segSum = 0.0;
 	private long segCnt = 0;
+	private long detFnCnt = 0;
+	private long detFpCnt = 0;
+	private long detTpCnt = 0;
 
 	public long getNumberOfAllSegCases() {
 		return segCnt;
@@ -26,13 +44,36 @@ public class DetSegCumulativeScores
 		return segCnt > 0 ? segSum / (double) segCnt : 0.0;
 	}
 
+	public long getNumberOfAllDetCases() {
+		return detFnCnt+detTpCnt;
+	}
+
+	public double getOverallDetScore() {
+		double convertingCost = detFnCnt*PENALTY_FN + detFpCnt*PENALTY_FP;
+		double buildingCost = getNumberOfAllDetCases()*PENALTY_FN;
+		return 1.0 - Math.min(convertingCost,buildingCost)/buildingCost;
+	}
+
+	/**
+	 * TODO: use directly once it is done better on the CTC measures side;
+	 * for now, consider {@link net.celltrackingchallenge.measures.TRA#penalty}
+	 */
+	static final double PENALTY_FN = 10; //how expensive is to create a new one
+	static final double PENALTY_FP = 1;  //how expensive is to remove wrong one
+
 	// --------------- section score ---------------
 	private double sectionSegSum = 0.0;
 	private long sectionSegCnt = 0;
+	private long sectionDetFnCnt = 0;
+	private long sectionDetFpCnt = 0;
+	private long sectionDetTpCnt = 0;
 
 	public void startSection() {
 		sectionSegSum = 0.0;
 		sectionSegCnt = 0;
+		sectionDetFnCnt = 0;
+		sectionDetFpCnt = 0;
+		sectionDetTpCnt = 0;
 	}
 
 	public long getNumberOfSectionSegCases() {
@@ -41,5 +82,15 @@ public class DetSegCumulativeScores
 
 	public double getSectionSegScore() {
 		return sectionSegCnt > 0 ? sectionSegSum / (double) sectionSegCnt : 0.0;
+	}
+
+	public long getNumberOfSectionDetCases() {
+		return sectionDetFnCnt+sectionDetTpCnt;
+	}
+
+	public double getSectionDetScore() {
+		double convertingCost = sectionDetFnCnt*PENALTY_FN + sectionDetFpCnt*PENALTY_FP;
+		double buildingCost = getNumberOfSectionDetCases()*PENALTY_FN;
+		return 1.0 - Math.min(convertingCost,buildingCost)/buildingCost;
 	}
 }
